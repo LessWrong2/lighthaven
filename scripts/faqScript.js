1153d582bece1ff60218d5c90aa2c93c4fde529a
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-03-19 15:31:38 -0700

    reordering script so that it runs after loading faq

--- scripts/faqScript.js
+++ scripts/faqScript.js
@@ -2,11 +2,13 @@
 
         function attachEventListenersToFaqItems() {
             const faqItems = document.querySelectorAll('.faq-item');
+            console.log(faqItems);
           
             faqItems.forEach(item => {
               const faqIconDiv = item.querySelector('.faqIcon');
               const faqIcon = item.querySelector('.fas');
               const answer = item.querySelector('.answer');
+              console.log(answer.textContent)
           
               faqIconDiv.addEventListener('click', () => {
                 answer.classList.toggle('hidden');
